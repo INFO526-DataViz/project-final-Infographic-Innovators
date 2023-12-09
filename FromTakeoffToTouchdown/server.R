09591ed4cda355643db36c36a8eb822328868c5d
Antonio <antonio@datalyticsintel.com>
2023-12-09 16:22:10 -0700

    TS Animated code added

--- FromTakeoffToTouchdown/server.R
+++ FromTakeoffToTouchdown/server.R
@@ -16,7 +16,7 @@ server <- function(input, output) {
   output$plot <- renderPlot({
     # Switch case to handle different plots
     switch(input$plotType,
-           "Time Series - Interactive" = { 
+           "Time Series - Interactive - Total Fatalies" = { 
              # Creating a dataset for the timeSeries plot
              flights_ntsb_timeseries <- flights_ntsb |>
                group_by(event_year) |>
@@ -52,8 +52,67 @@ server <- function(input, output) {
              ggplotly(p)
              
            },
-           "Plot 2" = { # Replace with your actual plot code or object
-             # Example: hist(rnorm(100))
+           "Time Series - Animated" = { # all 3 plots with label - animated 
+             
+             
+             fcdata <- flights_ntsb_timeseries |>
+               ungroup() |>
+               pivot_longer(cols = starts_with("total_"),
+                            names_to = "Category",
+                            values_to = "Count") |>
+               mutate(
+                 Category = case_when(
+                   Category == "total_fatalities" ~ "Fatalities",
+                   Category == "total_serious_injuries" ~ "Serious Injuries",
+                   Category == "total_minor_injuries" ~ "Minor Injuries"
+                 )
+               )
+             
+             
+             
+             # Creating the animated plot
+             animation_fcdata <- ggplot(fcdata,
+                                        aes(event_year,
+                                            Count,
+                                            group = Category,
+                                            color = Category)) +
+               geom_line(show.legend = FALSE) +
+               geom_segment(
+                 aes(xend = max(event_year) +
+                       0.1,
+                     yend = Count),
+                 linetype = 2,
+                 colour = 'grey',
+                 show.legend = FALSE
+               ) +
+               geom_point(size = 2,
+                          show.legend = FALSE) +
+               geom_text(
+                 aes(
+                   x = max(event_year) + 0.1,
+                   label = Category,
+                   color = "#000000"
+                 ),
+                 hjust = 0,
+                 show.legend = FALSE
+               ) +
+               transition_reveal(event_year) +
+               coord_cartesian(clip = 'off') +
+               theme(plot.title = element_text(size = 20)) +
+               labs(title = 'Aircraft Crash Statistics Over Time',
+                    y = 'Count',
+                    x = element_blank()) +
+               theme(plot.margin = margin(5.5, 40, 5.5, 5.5))
+             
+             # Animating the plot
+             animated_fcdata <- animate(
+               animation_fcdata,
+               fps = 10,
+               duration = 25,
+               width = 800,
+               height = 350,
+               renderer = gifski_renderer("images/animated_fcdata.gif")
+             )
            },
            "Plot 3" = { # Replace with your actual plot code or object
              # Example: boxplot(rnorm(100))
